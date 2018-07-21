aa3f117c124d4926ee31592dbd34a0385028e9cf
SVishC <chaitanyamvishnu@gmail.com>
2018-07-21 17:10:20 +0530

    File operations to store the students and retrieve later

--- yield_ex.py
+++ yield_ex.py
@@ -2,15 +2,13 @@ students = []
 
 
 def get_students_titlecase():
-    students_titlecase = []
     for student in students:
-        students_titlecase.append(student["name"].title())
-    return students_titlecase
+        yield student
 
 
 def print_students_titlecase():
-    students_titlecase = get_students_titlecase()
-    print(students_titlecase)
+    for students_titlecase in get_students_titlecase():
+        print(students_titlecase)
 
 
 
@@ -21,7 +19,7 @@ def add_student(name, student_id=121):
 
 def save_file(student_name): #Save student name to file
     try:
-        f=open("students.txt","a")
+        f=open("students2.txt","a")
         f.write(student_name+"\n") #adding new line because,we will read it later line by line(readlines())
         f.close()
     except Exception:
@@ -30,7 +28,7 @@ def save_file(student_name): #Save student name to file
 
 def read_file(): #reads the file and add the value to students calling add_student() method
     try:
-        f=open("students.txt","r")
+        f=open("students2.txt","r")
         for student in f.readlines():
             add_student(student) #Here we are adding only student name but not id,
             # bcz to add id we have to process Dictionaries,which needs some importing of Pickle module
